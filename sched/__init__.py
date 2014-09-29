48acdf5b82d2fd339bedc36247ade0d47265c40e
oyuFuentes <Oyuki Fuentes>
2014-09-29 13:33:29 -0500

    Incorporación manage
    Modificación de requerimiento.txt (flask-script)

---
+++ sched/__init__.py
@@ -0,0 +1,20 @@
+from flask import *
+import doctest
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def hello():
+	"""
+	
+	>>> hello()
+	'Hello, world!'
+	"""
+	return 'Hello, world!'
+
+
+if __name__ == "__main__":
+    doctest.testmod()    
+    app.run()
