554413fc18982a00d2cdc9d0cbb4308d03f62110
oyuFuentes <Oyuki Fuentes>
2014-09-29 18:10:39 -0500

    Validación flake
    Modificación para flake

--- sched/forms.py
+++ sched/forms.py
@@ -4,18 +4,18 @@ from wtforms.validators import Length, required
 
 
 class AppointmentForm(Form):
-	
-	title = TextField('Title', [Length(max=255)])
-	start = DateTimeField('Start', [required()])
-	end = DateTimeField('End')
-	allday = BooleanField('All Day')
-	location = TextField('Location', [Length(max=255)])
-	description = TextAreaField('Description')
+
+    title = TextField('Title', [Length(max=255)])
+    start = DateTimeField('Start', [required()])
+    end = DateTimeField('End')
+    allday = BooleanField('All Day')
+    location = TextField('Location', [Length(max=255)])
+    description = TextAreaField('Description')
 
 if __name__ == "__main__":
-	
-	 # Demonstration of a WTForms form by itself.
-	form = AppointmentForm()
-	print('Here is how a form field displays:')
-	print(form.title.label)
-	print(form.title)
+
+        # Demonstration of a WTForms form by itself.
+    form = AppointmentForm()
+    print('Here is how a form field displays:')
+    print(form.title.label)
+    print(form.title)
